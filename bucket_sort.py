6fc05634e3b60d878bfc481b2baf93f4b4932223
YSLucas <54868396+YSLucas@users.noreply.github.com>
2021-02-05 20:59:15 +0100

    Update bucket_sort.py

--- bucket_sort.py
+++ bucket_sort.py
@@ -7,14 +7,14 @@ def init(n, max_nmr, min_nmr):
     """
     one_dimensional = [random.randint(min_nmr, max_nmr) for i in range(n)]
     # two_dimensional = [ [x, [None for x in range(n-1)]] for x in range(10) ]
-    two_dimensional = [ [x, []] for x in range(10) ] # maak een roster aan met 9 rijen
+    two_dimensional = [ [x, []] for x in range(10) ] # maak een roster aan met 9 rijen. elke rij heeft een lijst waarin de getallen komen te staan
 
     return one_dimensional, two_dimensional
 
 
 def sorter(to_sort, bucket, tiental, slice_at):
     """ 
-    
+    Deze functie voert de sortering uit, en geeft een lijst terug met de bewerkingen na één gathering pass
     """
     new_list = []
 
@@ -45,6 +45,8 @@ def bucket_sort():
     
     to_sort, bucket = init(lenght_list, max_nmr, min_nmr)
 
+    print(f"De random gegenereerde lijst is: {to_sort}. \nDe lijst wordt nu gesorteerd..")
+
     clean_bucket = copy.deepcopy(bucket)    # creeer een deepcopy van een lege bucket om te hergebruiken
     tiental = 1                             # geeft aan of het algoritme bij een tiental, honderdtal etc. is
     slice_at = 1                            # variabele om de juiste cijfer in een getal aan te wijzen bij het sorteren
@@ -58,5 +60,6 @@ def bucket_sort():
         tiental *= 10   # na elke gathering pass ga je naar de volgende tiental. 10 -> 100 -> 1000
         slice_at += 1   # na elke gathering pass verschuift de focus één cijfer naar links. 
     
+    print(f"De lijst is succesvol gesorteerd! Dit is het resultaat: {to_sort}")
 
 bucket_sort()
